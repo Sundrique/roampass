386b2cdd4420dc33c9ac41b948706c61db65c26f
Alex Sandrovsky <alex.sandrovsky@hyperspots.com>
2014-01-22 01:19:45 +0300

    Hide operators except MTS

--- Roampass/src/main/java/com/sandrovsky/roampass/TemplatesList.java
+++ Roampass/src/main/java/com/sandrovsky/roampass/TemplatesList.java
@@ -12,24 +12,24 @@ public class TemplatesList extends HashMap<String, String> {
 
         //RU
         put("25001", "*137*N#");//MTS
-        put("25005", "*147*N#");//ETK
-        put("25012", "*117*N#");//Baykalwestcom
-        put("25015", "*114*N#");//SMARTS
-        put("25020", "*147*+N#");//Tele2
+        //put("25005", "*147*N#");//ETK
+        //put("25012", "*117*N#");//Baykalwestcom
+        //put("25015", "*114*N#");//SMARTS
+        //put("25020", "*147*+N#");//Tele2
         //UA
-        put("25501", "*111*+N#");//MTS
-        put("25503", "*105*N#");//Kyivstar
-        put("25506", "*131*N#");//life:)
+        //put("25501", "*111*+N#");//MTS
+        //put("25503", "*105*N#");//Kyivstar
+        //put("25506", "*131*N#");//life:)
         //KZ
-        put("40101", "*137*N#");//Beeline
-        put("40177", "*130*N#");//Tele2
+        //put("40101", "*137*N#");//Beeline
+        //put("40177", "*130*N#");//Tele2
         //RS
-        put("22001", "*123*00N#");//Telenor
-        put("22003", "*102*N#");//mt:s
+        //put("22001", "*123*00N#");//Telenor
+        //put("22003", "*102*N#");//mt:s
         //ME
-        put("29702", "*124*0N#");//T-Mobile
-        put("29703", "*102*+N#");//m:tel
-        put("29704", "*124*0N#");//T-Mobile (?)
+        //put("29702", "*124*0N#");//T-Mobile
+        //put("29703", "*102*+N#");//m:tel
+        //put("29704", "*124*0N#");//T-Mobile (?)
     }
 
     public Template getByOperatorId(String operatorId) {
